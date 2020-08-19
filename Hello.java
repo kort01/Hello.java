4856b59fc3767e5f805540a212798170e8cff900
kort01 <69913244+kort01@users.noreply.github.com>
2020-08-19 11:44:18 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/*****************
+Kortney Summers
+08/17/2020
+255-004
+****************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Kortney>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Intro to java>");
+		System.out.println("<08/17/2020>");
+		System.out.println("<255-004>");
+		System.out.println();
+	}
+}
